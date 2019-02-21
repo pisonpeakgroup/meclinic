2abe8cb7f882202b6f1affc4fc5a9e205b3c3347
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-02-21 15:43:31 +0100

    added the user addtion controller action

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,38 @@
+/**
+ * Created by bolorundurowb on 2/21/2019
+ */
+
+const User = require('./../models/User');
+
+const usersController = {
+    add: function (req, res) {
+        const body = req.body;
+
+        if (!body.email) {
+            res.status(400).send({
+                message: 'An email address is required.'
+            });
+        } else if (!body.password) {
+            res.status(400).send({
+                message: 'A password is required.'
+            })
+        } else {
+            const user = new User(body);
+            user.save((err) => {
+                if (err) {
+                    res.status(500).send({
+                        message: 'An error occurred.',
+                        error: err
+                    });
+                } else {
+                    res.status(200).send({
+                        user: user
+                    });
+                }
+            });
+        }
+    }
+};
+
+module.exports = usersController;
+ 
